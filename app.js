870d9ebb67a12bbb2869921001c1203cd8985602
Daniel Cuevas <danny89c33@gmail.com>
2020-05-04 21:17:27 -0700

    created array function

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ function inRange() {
         document.getElementById('primes').innerText = "";
     }
     else if(num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <=100){
-        
+        findPrimes(num1, num2);
+    }
+}
+
+function findPrimes(firstInput, secondInput){
+    var numArray = []
+    const smallNum = 0
+    const largeNum = 0
+    if(firstInput < secondInput){
+        firstInput = smallNum
+        secondInput = largeNum
+    }
+    else if(firstInput > secondInput){
+        firstInput = largeNum
+        secondInput = smallNum
+    }
+
+    while(smallNum <= largeNum){
+        if(primeNum(smallNum) == true){
+            numArray.push(smallNum)
+            smallNum += 1
+        }
+        else if(primeNum(smallNum) == false){
+            smallNum += 1
+        }
     }
 }
