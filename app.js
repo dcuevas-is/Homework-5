0950931eb32660e9fbae77330e1a40299fad1c42
Daniel Cuevas <danny89c33@gmail.com>
2020-05-04 20:59:43 -0700

    created inRange function

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const num1 = parseInt(document.getElementById('num1').value);
 const num2 = parseInt(document.getElementById('num2').value);
 const buttonElement = document.getElementById('submit');
 
-/*function checkNums(num1) {
-    if(num1 >= 2 && num1 <= 100)
+function inRange() {
+    if(num1 < 2 || num1 > 100 || num2 < 2 || num2 > 100){
+        document.getElementById('error').innerText = "Invalid input, please try again";
+        document.getElementById('primes').innerText = "";
+    }
+    else if(num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <=100){
+        
+    }
 }
-*/
-function range(start, end) {
-    return Array(end - start + 1).fill().map((_, idx) => start + idx)
-}
-var result = range(num1.value, num2.value);
-
-
-buttonElement.addEventListener("click", range);
-console.log(result);
-/*function findPrimes(var1, var2) {
-
-}*/
